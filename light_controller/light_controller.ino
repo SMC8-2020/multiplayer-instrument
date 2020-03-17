65e740799e891558cad659a4b17fcf8d613aab23
Juan Alonso <juan@neuronasmuertas.com>
2020-03-17 12:25:59 +0100

    Simple demo scenes

--- light_controller/light_controller.ino
+++ light_controller/light_controller.ino
@@ -1,13 +1,27 @@
+#include <FastLED.h>
 #include <Bounce2.h>
 #include <Adafruit_NeoPixel.h>
 
+
+
+//Defs
 #define BUTTON_PIN D3
 #define LED_PIN    D1
 
 #define LED_COUNT   1
+#define MAX_STATE   8
 
+
+
+//Hardware
 Bounce button = Bounce(BUTTON_PIN, 15);
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+
+
+int state;
+boolean buttonPressed;
+
 
 void setup() {
 
@@ -20,6 +34,9 @@ void setup() {
 
   Serial.begin(9600);
 
+  state = 0;
+  buttonPressed = false;
+
 }
 
 
@@ -29,16 +46,51 @@ void loop() {
   button.update();
 
   if (button.fallingEdge()) {
+
+    buttonPressed = true;
+
     digitalWrite(LED_BUILTIN, LOW);
-    strip.fill(strip.Color(0, 0, 0, 255));
-    strip.show();
+    strip.fill(strip.Color(0, 0, 255, 0));
   }
 
   if (button.risingEdge()) {
+
+    buttonPressed = false;
+    state++;
+    if (state >= MAX_STATE) {
+      state = 0;
+    }
+
     digitalWrite(LED_BUILTIN, HIGH);
     strip.fill(strip.Color(0, 0, 0, 0));
-    strip.show();
   }
+
+  if (!buttonPressed) {
+    int color = 0;
+    switch (state) {
+      case 0: color = (millis() % 512) >> 1;
+        break;
+      case 1: color = (millis() % 1024) >> 2;
+        break;
+      case 2: color = sin8((millis() % 512) >> 1);
+        break;
+      case 3: color = sin8((millis() % 1024) >> 2);
+        break;
+      case 4: color = ((millis() % 512) >> 1) > 128 ? 255 : 0;
+        break;
+      case 5: color = ((millis() % 1024) >> 2) > 128 ? 255 : 0;
+        break;
+      case 6: color = inoise8(millis());
+        break;
+      case 7: color = inoise8(millis()/2);
+        break;        
+    }
+    strip.fill(strip.Color(0, 0, 0, color));
+  }
+
+  strip.show();
+
+
 
   delay(25);
 }
