e44757688191231ccfbcab63ec9450de5ceea5f5
Sophus Benee Olsen <sopusolsen@hotmail.com>
2020-04-27 12:50:07 +0200

    added JRE to distribution package

--- distribution_package/launchscript.sh
+++ distribution_package/launchscript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 path="$(pwd)/applications"
 dependency="examplePd"
-javajdk="$(pwd)/javaruntime/adoptopenjdk-8.jdk/Contents/Home"
-javart="${javajdk}/jre/bin/java"
+javart="$(pwd)/javaruntime/jre/bin/java"
 "${javart}" -jar ./launcher/applauncher.jar "${path}" "${dependency}"
